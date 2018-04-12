05c5f22dda8b8cb3955d022550f0fa12939f0c3b
awalton3 <awalton3@nd.edu>
2018-04-12 17:56:20 -0400

    added forms: lesson 4

---
+++ Angular Pro/Forms Lesson3/couponFormat.js
@@ -0,0 +1,33 @@
+function couponFormat () {
+    
+    return {
+        
+        require: 'ngModel',
+        link: function ($scope, $elem, $attrs, $ctrl) {
+            
+            var ngModelCtrl = $ctrl;
+            
+            //manipulate value before it's returned (model to view)
+            ngModelCtrl.$formatters.unshift(function (value) {
+                return value.replace(/-/g, '_').toUpperCase()
+            }); 
+            
+             //view to model
+            ngModelCtrl.$parsers.unshift(function (value) {
+                return value.replace(/_/g, '-').toLowerCase()
+            }); 
+            
+            ngModelCtrl.$validators.coupon = function (modelValue, viewValue) {
+                
+                var COUPON_REGEX = /[A-Z]+\_\d{2}/; //view value
+                return COUPON_REGEX.test(viewValue); 
+                
+            };
+            
+        }  
+    }  
+}
+
+angular 
+    .module('app')
+    .directive('couponFormat',couponFormat)
